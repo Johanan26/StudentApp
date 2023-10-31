4f1861c80433298bebefa36a549591bd0e651c68
Johanan26 <120645993+Johanan26@users.noreply.github.com>
2023-10-31 20:46:40 +0000

    finished student app code also added 3 student info

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -2,14 +2,12 @@ package ie.atu;
 
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args)
-    {
-    String firstName;
-    String Name3, Email3, Course3;
-    Scanner scanner = new Scanner(System.in);
-    Student user1 = new Student();
-
-    //Student 1
+    public static void main(String[] args) {
+        String firstName;
+        String Name3,Email3,Course3;
+        Scanner scanner = new Scanner(System.in);
+        Student user1 = new Student();
+        //Student 1
         System.out.print("STUDENT 1\n");
         System.out.print("Enter Student Name: ");
         user1.setName(scanner.nextLine());
@@ -18,8 +16,7 @@ public class Main {
         System.out.print("Enter Student Course: ");
         user1.setCourse(scanner.nextLine());
         System.out.print("\n");
-
-//Student 2
+        //Student 2
         System.out.print("STUDENT 2\n");
         System.out.print("Enter Student Name: ");
         firstName = scanner.nextLine();
@@ -29,9 +26,19 @@ public class Main {
         System.out.print("Enter Student Course: ");
         user2.setCourseBName(scanner.nextLine());
         System.out.print("\n");
-
-    //displays all information gathered
+        //Student 3
+        System.out.print("STUDENT 3\n");
+        System.out.print("Enter Student Name: ");
+        Name3 = scanner.nextLine();
+        System.out.print("Enter Student Email: ");
+        Email3 = scanner.nextLine();
+        System.out.print("Enter Student Course: ");
+        Course3 = scanner.nextLine();
+        Student user3 = new Student(Name3,Email3,Course3);
+        System.out.print("\n");
+        //displays all information gathered
         user1.displayInfo();
         user2.displayInfoB();
-}
+        user3.displayInfoC();
     }
+}
